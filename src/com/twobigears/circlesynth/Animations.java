c2187aa299450e71d272cb812311d3ef02f510d6
Varun Nair <nothingtokeep@gmail.com>
2013-10-07 19:05:59 +0100

    Toolbar revamped!

--- src/com/twobigears/circlesynth/Animations.java
+++ src/com/twobigears/circlesynth/Animations.java
@@ -1,28 +1,48 @@
 package com.twobigears.circlesynth;
 
+import android.util.Log;
+import android.view.animation.AccelerateInterpolator;
 import android.view.animation.Interpolator;
 import android.view.animation.OvershootInterpolator;
 
 public class Animations {
-	
+
 	private float value;
 	public float totalFrames, animateValue, increment;
-	
-	Interpolator animationInterpolator;
-	
+
+	Interpolator overshootInterpolator;
+	Interpolator accelerateInterpolator;
+
 	Animations(float lengthInFrames) {
 		totalFrames = lengthInFrames;
-		animationInterpolator = new OvershootInterpolator(1.7f);
+		overshootInterpolator = new OvershootInterpolator(1.7f);
+		accelerateInterpolator = new AccelerateInterpolator();
 		reset();
 	}
-	
+
 	public void reset() {
 		value = 0;
 		increment = 1;
+		animateValue = 0;
 	}
-	
+
 	public void animate() {
 		value += increment;
-		animateValue = animationInterpolator.getInterpolation(Math.min(value/totalFrames, 1f));
-	}	
+		animateValue = overshootInterpolator.getInterpolation(Math.min(value
+				/ totalFrames, 1f));
+	}
+
+	public void accelerateUp() {
+		value = Math.min(value, totalFrames);
+		value += increment;
+		animateValue = overshootInterpolator.getInterpolation(value
+				/ totalFrames);
+	}
+
+	public void accelerateDown() {
+		value = Math.max(value, 0);
+		value -= increment;
+		animateValue = overshootInterpolator.getInterpolation(Math.max(value
+				/ totalFrames, 0f));
+	}
 }
